0048b2b83cca59dcff641491ab0643ba6572e3a9
Sonadi <144614600+Sonadi@users.noreply.github.com>
2026-03-24 21:56:47 +0530

    update1

--- routes/notificationRoute.js
+++ routes/notificationRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const sendMail = require('../sendMail');
 
-// 📦 Order Placed.
+// 📦 Order Placed
 router.post('/order-placed', async (req, res) => {
   const { customerEmail } = req.body;
   await sendMail(
